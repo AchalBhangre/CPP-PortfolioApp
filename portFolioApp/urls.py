5ec345ae4bc3a1a2684835abe748cac461f58742
EC2 Default User <ec2-user@ip-172-31-22-7.eu-west-1.compute.internal>
2023-04-23 08:44:55 +0000

    unwanted files

--- portFolioApp/urls.py
+++ portFolioApp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('portfolio_list', views.portfolio_list, name='portfolio_list'),
     path('registrations/<int:registration_id>/', views.registration_details, name='registration_details'),
    path('portfolio_detail/<int:portfolio_id>/', views.portfolio_detail, name='portfolio_detail'),
-   path('portfolio-image/<int:image_id>/', views.portfolio_image_details, name='portfolio_image_details')
+  # path('portfolio-image/<int:image_id>/', views.portfolio_image_details, name='portfolio_image_details')
 
 ]
 
