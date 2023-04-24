8cdcac5e8d161be9e9c77c78430a319aeaa7c944
EC2 Default User <ec2-user@ip-172-31-22-7.eu-west-1.compute.internal>
2023-04-24 15:29:45 +0000

    ratio changes

--- portFolioApp/utils/email.py
+++ portFolioApp/utils/email.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def send_confirmation_email(recipient_email):
-    subject = 'Registration Confirmation for Portfolio Site'
+    subject = 'Registration Confirmation with Portfolio Site'
     message = 'Thank you for registering with Us!, Hope you enjoy the Site. You will be receving mails for every deatils when your profile gets updated'
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [recipient_email]
