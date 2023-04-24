738bde326d7be40a6b22578acdf20ea0facddd98
EC2 Default User <ec2-user@ip-172-31-22-7.eu-west-1.compute.internal>
2023-04-24 08:58:37 +0000

    ebs deployment file

--- cppPortfolio/urls.py
+++ cppPortfolio/urls.py
@@ -18,11 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('portFolioApp/', include('portFolioApp.urls')),
     path('admin/', admin.site.urls),
+    path('signin/',auth_views.LoginView.as_view(
+        template_name='signin.html'),name='signin'),
+    path('signout/',auth_views.LogoutView.as_view(
+        template_name='signout.html'),name='signout'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
