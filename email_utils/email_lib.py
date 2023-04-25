1b5e2f001d133322855edd0bfeb71398a311f977
EC2 Default User <ec2-user@ip-172-31-22-7.eu-west-1.compute.internal>
2023-04-25 10:06:26 +0000

    adding emailmsg

--- email_utils/email_lib.py
+++ email_utils/email_lib.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 def send_confirmation_email(recipient_email):
     subject = 'Registration Confirmation with Portfolio Site'
-    message = 'Thank you for registering with Us!, Hope you enjoy the Site. You will be receving mails for every deatils when your profile gets updated'
+    message = 'Thank you for registering with Us!, Hope you enjoy the Site.'
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [recipient_email]
     send_mail(subject, message, from_email, recipient_list)
